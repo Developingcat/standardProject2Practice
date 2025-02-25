3e9c5d47bc6852b422dddb0e29594b086a0e1100
Hannah <148793520+Developingcat@users.noreply.github.com>
2025-02-25 15:42:53 +0000

    It's DONE I never want to see this again

--- standardProject2Practice.py
+++ standardProject2Practice.py
@@ -1,30 +1,53 @@
+print("##################################")
+print("##                              ##")
+print("##      Welcome to loan         ##")
+print("##     Payment Calculator!      ##")
+print("##                              ##")
+print("##################################")
 
-#################### - Function definations 
 
-def get_principal():
-    while True: 
-        variable = input("Please enter the loan principal amount($): ")
-        if variable >= -1: 
-            return get_principal
-        else: 
-            print("Please re-enter the loan principal amount($): ")
-            # except ValueError:
-            return 
+
+
+
+
 ################- Variable 
 
-principal = float(get_principal())
 
-annual_rate = get_annual_rate(float)
+principal = float(input("Enter the loan principal amount($): "))
 
-years = get_years(int)
+annual = float(input("Enter the annual interest rate: (in %, e.g, 5.0 for 5%, or 6.5 for 6.5% ): "))
 
-monthly_payment = calculate_monthly_payment(principal, annual_rate, years)
+years = int(input("Enter the amount of years: "))
 
 
+# For reference strong credits to the pink one; interest_rate = intrest / 100 / 12, months = loan * 12, and then an if else for if there's interest 
+
+annual_rate = annual / 100 / 12
+months = years * 12
+
+# Note to self, work on the math before tommorrow, use as a study guide reference 
+
+if annual_rate > 0:
+    monthly_payment = (principal * annual_rate) / (1 -(1 + annual_rate) ** -months) # Try to keep the formula in mind,
+else:
+    monthly_payment = principal / months 
+
+total_payment = monthly_payment * months  
+
+print("Loan Payment Summary!")
+print("=====================")
+print(f"Loan Principal: ${principal}")
+print("========---------")
+print(f"Annual Interest Rate!: ${annual}")
+print("========---------")
+print(f"Loan Term: {years} years")
+print("========---------")
+print(f"Total Payment: ${total_payment:.2f}")
 ################## - Testing area - remove in final
 
 
-get_principal()
+
+
 
 
 
@@ -36,12 +59,6 @@ get_principal()
 ########################
 
 
-print("##################################")
-print("##                              ##")
-print("##      Welcome to loan         ##")
-print("##     Payment Calculator!      ##")
-print("##                              ##")
-print("##################################")
 
 
 
